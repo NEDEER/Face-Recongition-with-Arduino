2cefc497d22cc811cf84f309bd227befd4c313dc
Neder <bougossanader1s3@gmail.com>
2024-07-31 23:44:13 +0100

    apload files

---
+++ Face_REC/Face_REC.ino
@@ -0,0 +1,16 @@
+#include <cvzone.h>
+
+SerialData serialData(2, 1); // num0fValsREc,digitsPerValRec
+int valsRec[2];
+void setup() {
+    pinMode(13,OUTPUT);
+    pinMode(12,OUTPUT);
+    serialData.begin();    
+}
+
+void loop() {
+  serialData.Get(valsRec);
+  digitalWrite(13,valsRec[0]); // green led
+  digitalWrite(12,valsRec[1]); // red led
+  delay(10);
+}
